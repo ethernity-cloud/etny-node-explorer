497300987ab27a10ec4818e78b11aa3458a20bb1
Mikheil <suxoza@gmail.com>
2022-08-11 15:20:40 +0400

    first refactoring, same behavior

--- nodes_reader.py
+++ nodes_reader.py
@@ -12,122 +12,143 @@ import csv
 from node import Node
 
 # config section
-config = configparser.ConfigParser()
-config.read('config.env')
-httpProvider = config['DEFAULT']['HttpProvider']
-contract = config['DEFAULT']['ContractAddress']
-indexFile = config['DEFAULT']['IndexFile']
-nodesFile = config['DEFAULT']['CSVFile']
 
-w3 = Web3(Web3.HTTPProvider(httpProvider))
-if version.parse(web3.__version__) < version.parse('5.0.0'):
-    w3.middleware_stack.inject(geth_poa_middleware, layer=0)
-else:
-    w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+class NotFoundException(Exception):
+    pass
 
+class Reader:
 
-def read_contract_abi():
-    try:
-        contract_file = open(os.path.dirname(os.path.realpath(__file__)) + '/entyContract.abi')
-        abi = contract_file.read()
-        contract_file.close()
-        return abi
-    except Exception as e:
-        print(e)
-        return None
+    _w3 = None
+    _contract = None
+    _indexFile = None
+    _nodesFile = None
+
+    def __init__(self) -> None:
+        self._baseConfig()
 
 
-def read_index_content():
-    index_dict = dict()
-    if os.path.exists(indexFile):
-        index = open(indexFile)
-        index_dict = json.load(index)
-        index.close()
+        self._action()
 
-    return index_dict
+    def _baseConfig(self) -> None:
+        config = configparser.ConfigParser()
+        config.read('config.env')
+        httpProvider = config['DEFAULT']['HttpProvider']
 
+        self._contract = config['DEFAULT']['ContractAddress']
+        self._indexFile = config['DEFAULT']['IndexFile']
+        self._nodesFile = config['DEFAULT']['CSVFile']
 
-def read_csv_file(file):
-    _rows = dict()
-    if not os.path.exists(file):
-        return _rows
+        self._w3 = Web3(Web3.HTTPProvider(httpProvider))
+        if version.parse(web3.__version__) < version.parse('5.0.0'):
+            self._w3.middleware_stack.inject(geth_poa_middleware, layer=0)
+        else:
+            self._w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
-    with open(file) as fileContent:
-        for line in csv.reader(fileContent, dialect="excel-tab"):
-            _row = Node(line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9],
-                        line[10])
-            _rows[_row.address] = _row
+    def _action(self):
+        etnyContract = self._w3.eth.contract(address=self._contract, abi=self.__read_contract_abi())
+        firstContractTx = "0x90eeb0a0680034c8c340dfa60b773ed77b060d6a966596059610981486d312f4"
+        print("Connected...")
 
-    return _rows
+        blockNumber = self._w3.eth.getTransaction(firstContractTx).blockNumber
+        timestamp = self._w3.eth.getBlock(blockNumber).timestamp
+        currentBlock = self._w3.eth.blockNumber
 
+        print('block number', blockNumber)
+        print('timestamp', timestamp)
+        print('current block number', currentBlock)
 
-etnyContract = w3.eth.contract(address=contract, abi=read_contract_abi())
-firstContractTx = "0x90eeb0a0680034c8c340dfa60b773ed77b060d6a966596059610981486d312f4"
-print("Connected to: ", httpProvider)
+        values = self.__read_index_content()
+        startBlockNumber = blockNumber + 10
+        if len(values.keys()) > 0:
+            startBlockNumber = values[max(values, key=values.get)]
 
-blockNumber = w3.eth.getTransaction(firstContractTx).blockNumber
-timestamp = w3.eth.getBlock(blockNumber).timestamp
-currentBlock = w3.eth.blockNumber
+        for i in range(startBlockNumber, currentBlock, 10):
+            currentCounter = etnyContract.functions._getDPRequestsCount().call(block_identifier=i)
+            if currentCounter not in values:
+                values[currentCounter] = i
+                if currentCounter % 10 == 0:
+                    with open(self._indexFile, 'w') as convert_file:
+                        convert_file.write(json.dumps(values))
+            print('reading block: ', i)
 
-print('block number', blockNumber)
-print('timestamp', timestamp)
-print('current block number', currentBlock)
+        # write json back
+        self.__write_index_content(values)
 
-values = read_index_content()
-startBlockNumber = blockNumber + 10
-if len(values.keys()) > 0:
-    startBlockNumber = values[max(values, key=values.get)]
-
-for i in range(startBlockNumber, currentBlock, 10):
-    currentCounter = etnyContract.functions._getDPRequestsCount().call(block_identifier=i)
-    if currentCounter not in values:
-        values[currentCounter] = i
-        if currentCounter % 10 == 0:
-            with open(indexFile, 'w') as convert_file:
-                convert_file.write(json.dumps(values))
-    print('reading block: ', i)
-
-with open(indexFile, 'w') as convert_file:
-    convert_file.write(json.dumps(values))
-
-values = read_index_content()
-nodes = read_csv_file(nodesFile)
-
-startingCount = 0
-if len(nodes.keys()) > 0:
-    startingCount = etnyContract.functions._getDPRequestsCount().call(block_identifier=startBlockNumber)
-
-endingCount = etnyContract.functions._getDPRequestsCount().call()
+        # get json again
+        values = self.__read_index_content()
 
 
-def get_timestamp_from_request(request_id):
-    while str(request_id) not in values.keys():
-        request_id = request_id - 1
+        nodes = self.__read_csv_file(self._nodesFile)
 
-    block_number = values[str(request_id)]
-    time_stamp = w3.eth.getBlock(block_number).timestamp
+        startingCount = 0
+        if len(nodes.keys()) > 0:
+            startingCount = etnyContract.functions._getDPRequestsCount().call(block_identifier=startBlockNumber)
 
-    return time_stamp
+        endingCount = etnyContract.functions._getDPRequestsCount().call()
+
+        node_index = len(nodes.keys()) + 1
+        for i in range(startingCount, endingCount):
+            request = etnyContract.functions._getDPRequest(i).call()
+            print("reading request ", i)
+            if request[0] not in nodes:
+                node = Node(node_index, request[0], request[1], request[2], request[3], request[4], request[5], request[6],
+                            request[7],
+                            self.__get_timestamp_from_request(values, i), self.__get_timestamp_from_request(values, i))
+                nodes[request[0]] = node
+                node_index = node_index + 1
+            else:
+                nodes[request[0]].last_updated = self.__get_timestamp_from_request(values, i)
+
+        if len(nodes) > 0:
+            with open(self._nodesFile, 'w',newline='') as output_file:
+                writer = csv.writer(output_file, dialect="excel-tab")
+                for k, row in nodes.items():
+                    writer.writerow(
+                        [row.no, row.address, row.cpu, row.memory, row.storage, row.bandwith, row.duration, row.status,
+                        row.cost, row.created_on, row.last_updated])
+
+    def __read_contract_abi(self) -> str:
+        try:
+            with open(os.path.dirname(os.path.realpath(__file__)) + '/entyContract.abi') as r:
+                return r.read()
+        except Exception as e:
+            return None
+
+    def __read_index_content(self) -> dict:
+        if os.path.exists(self._indexFile):
+            with open(self._indexFile) as r:
+                return json.loads(r)
+        return {}
+
+    def __write_index_content(self, jsonContent) -> dict:
+        with open(self._indexFile, "w") as w:
+            w.write(json.dumps(jsonContent))
+
+    def __read_csv_file(self, file) -> None:
+        rows = {}
+        try:
+            if not os.path.exists(file):
+                raise NotFoundException
+
+            with open(file) as fileContent:
+                for line in csv.reader(fileContent, dialect="excel-tab"):
+                    _row = Node(line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9], line[10])
+                    rows[_row.address] = _row
+        except Exception as e:
+            print(e)
+        finally:
+            return rows
+
+    def __get_timestamp_from_request(self, values, request_id):
+        while str(request_id) not in values.keys():
+            request_id = request_id - 1
+
+        block_number = values[str(request_id)]
+        time_stamp = self._w3.eth.getBlock(block_number).timestamp
+
+        return time_stamp
 
 
-node_index = len(nodes.keys()) + 1
-for i in range(startingCount, endingCount):
-    request = etnyContract.functions._getDPRequest(i).call()
-    print("reading request ", i)
-    if request[0] not in nodes:
-        node = Node(node_index, request[0], request[1], request[2], request[3], request[4], request[5], request[6],
-                    request[7],
-                    get_timestamp_from_request(i), get_timestamp_from_request(i))
-        nodes[request[0]] = node
-        node_index = node_index + 1
-    else:
-        nodes[request[0]].last_updated = get_timestamp_from_request(i)
 
-if len(nodes) > 0:
-    with open(nodesFile, 'w',
-              newline='') as output_file:
-        writer = csv.writer(output_file, dialect="excel-tab")
-        for k, row in nodes.items():
-            writer.writerow(
-                [row.no, row.address, row.cpu, row.memory, row.storage, row.bandwith, row.duration, row.status,
-                 row.cost, row.created_on, row.last_updated])
+if __name__ == '__main__':
+    Reader()
