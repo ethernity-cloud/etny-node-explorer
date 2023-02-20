549a620cbc8a93bbd4e67419dfcb3620ef640d25
apoeana <103731835+apoeana@users.noreply.github.com>
2023-02-20 16:50:25 +0200

    Update create_mysql_database.sh

--- create_mysql_database.sh
+++ create_mysql_database.sh
@@ -10,7 +10,7 @@ mysql -e "create user if not exists ${DB_USERNAME}@${DB_HOST_ALIAS} identified b
 mysql -e "grant all on ${DB_DATABASE}.* to ${DB_USERNAME}@${DB_HOST_ALIAS};";
 mysql -e "flush privileges";
 mysql -e "use ${DB_DATABASE}";
-mysql -e "SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));"
+mysql -e "SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode, 'ONLY_FULL_GROUP_BY', ''))";
 echo "database have been created..."
 # create tables 
 
